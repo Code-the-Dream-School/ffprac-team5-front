bcff9d04892267db9f9b323127a95bd45610597e
anniethewebcoder <anniethewebcoder@gmail.com>
2024-04-23 02:12:16 -0400

    background image

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import styles from './../css/Login.module.css'
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -21,29 +22,43 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          autoComplete="current-email"
-        />
+    <div className={styles.container}>
+      <div className={styles.left}>
       </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          autoComplete="current-password"
-        />
+
+      <div className={styles.right}>
+        <h1>Recipe Finder</h1>
+        <h6>A tool to help you use up those leftover ingredients</h6>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <h2>Log In or Sign Up for Free</h2>
+          <div className={styles.field}>
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              autoComplete="current-email"
+            />
+          </div>
+          <div className={styles.field}>
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              autoComplete="current-password"
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <div className={styles.field}>
+          <div>Create New Account</div>
+          <div>Continue as Guest</div>
+        </div>
       </div>
-      <button type="submit">Login</button>
-    </form>
+    </div>
   );
 };
 
