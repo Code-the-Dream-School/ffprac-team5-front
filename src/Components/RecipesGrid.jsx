51ae5924f93c9b7bc308296383ab78f0110042dc
kendrick <kmg3795@yahoo.com>
2024-04-25 15:23:18 -0500

    Recipe Page template/layout with corresponding components and CSS elements

--- src/Components/RecipesGrid.jsx
+++ src/Components/RecipesGrid.jsx
@@ -1,7 +1,43 @@
-export default function RecipesGrid(){
+import RecipeCard from "./RecipeCard";
+import "./RecipeGrid.css";
+import React from "react";
+
+export default function RecipesGrid() {
+  const testRecipes = [
+    {
+      name: "Spaghetti",
+      imageUrl: "src/assets/images/spaghet.jpg",
+    },
+    {
+      name: "Italian Pizza",
+      imageUrl: "src/assets/images/original_italian_pizza.jpg",
+    },
+    {
+      name: "Loaded Cheeseburger",
+      imageUrl: "src/assets/images/burger.jpg",
+    },
+
+    {
+      name: "Teriyaki Chicken",
+      imageUrl: "src/assets/images/teriyakichicken.jpg",
+    },
+
+    {
+      name: "Fried Catfish",
+      imageUrl: "src/assets/images/friedcatfish.jpg",
+    },
+  ];
+
   return (
-    <div>
-      <p>Recipes grid</p>
+    <div id="recipeGridContainer">
+      <h3 id="gridHeader">
+        <b>Similar Recipes</b>
+      </h3>
+      {testRecipes.map((recipe, index) => (
+        <div key={index}>
+          <RecipeCard name={recipe.name} imageUrl={recipe.imageUrl} />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
