ed7b1d65db456d60c55d17957836588dfe84de6f
Ashley Bocanegra <ashleybocanegra1196@gmail.com>
2024-04-29 23:04:13 -0400

    Search_Page_UI

--- src/shared-components/RecipesGrid.jsx
+++ src/shared-components/RecipesGrid.jsx
@@ -2,38 +2,13 @@ import RecipeCard from "./RecipeCard";
 import "./RecipeGrid.css";
 import React from "react";
 
-export default function RecipesGrid() {
-  const testRecipes = [
-    {
-      name: "Spaghetti",
-      imageUrl: "src/assets/images/spaghet.jpg",
-    },
-    {
-      name: "Italian Pizza",
-      imageUrl: "src/assets/images/original_italian_pizza.jpg",
-    },
-    {
-      name: "Loaded Cheeseburger",
-      imageUrl: "src/assets/images/burger.jpg",
-    },
-
-    {
-      name: "Teriyaki Chicken",
-      imageUrl: "src/assets/images/teriyakichicken.jpg",
-    },
-
-    {
-      name: "Fried Catfish",
-      imageUrl: "src/assets/images/friedcatfish.jpg",
-    },
-  ];
-
+export default function RecipesGrid({ recipes }) {
   return (
     <div id="recipeGridContainer">
       <h3 id="gridHeader">
         <b>Similar Recipes</b>
       </h3>
-      {testRecipes.map((recipe, index) => (
+      {recipes.map((recipe, index) => (
         <div key={index}>
           <RecipeCard name={recipe.name} imageUrl={recipe.imageUrl} />
         </div>
